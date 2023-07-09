6b331eecb5ed2cf5870ffb6201127ec4e04bbafb
Deepanshu <deepanshuj952@gmail.com>
2023-07-10 02:23:19 +0530

    first commit

---
+++ src/joke/index.jsx
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+import VoiceSelector from "./components/VoiceSelector";
+import {speak} from "./utils/speak";
+import {getJoke} from "./api/joke";
+import "./index.css";
+import {useKeyDown} from "./hooks/useKeyDown";
+import robotGif from './robot.gif'
+
+const Index = () => {
+    const [selectedVoice, setSelectedVoice] = useState(147);
+    const [joke, setJoke] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const tellMeAJoke = async () => {
+        setLoading(true);
+        const joke = await getJoke();
+        setJoke(joke);
+        speak(`${joke.setup} ${joke.punchline}`, selectedVoice);
+        setLoading(false);
+    }
+
+    useKeyDown(() => tellMeAJoke(), ['j']);
+
+    return (
+        <div className='center'>
+            <img src={robotGif} alt='robot' />
+            <VoiceSelector selected={selectedVoice} setSelected={setSelectedVoice} />
+            {!!joke && (
+             <div>
+                <p>{joke.setup}</p>
+                <p>{joke.punchline}</p>
+                </div>
+                )}
+            {loading ? <p>Loading...</p> : <button onClick={tellMeAJoke}>Tell me a joke!</button>}
+        </div>
+    );
+};
+
+export default Index;
